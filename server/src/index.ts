40369dce474aad140177150f17d991aa3ab72c62
Rafael Silva <rafael.silva-vergara@ehsai.com>
2024-02-20 10:31:36 -0800

    basic express app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AppDataSource } from "./data-source"
-import "reflect-metadata"
+import { AppDataSource } from "./data-source";
+import "reflect-metadata";
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { PromptTemplate } from '@langchain/core/prompts';
+import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
-import { StringOutputParser } from 'langchain/schema/output_parser';
+import { StringOutputParser } from "langchain/schema/output_parser";
 import { DataSource } from "typeorm";
 import { SqlDatabase } from "langchain/sql_db";
 
-
-async function getResponse() {  
-    const datasource = new DataSource({
+async function getResponse() {
+  const datasource = new DataSource({
     type: "postgres",
-    host: 'localhost',
+    host: "localhost",
     synchronize: true,
     username: "postgres",
     port: 5432,
-    password: 'admin',
+    password: "admin",
     database: "dvdrental",
   });
 
-    const db = await SqlDatabase.fromDataSourceParams({
+  const db = await SqlDatabase.fromDataSourceParams({
     appDataSource: datasource,
   });
-  
+
   const model = new ChatOllama({
-    baseUrl: "http://localhost:11434", 
-    model: "llama2", 
+    baseUrl: "http://localhost:11434",
+    model: "llama2",
   });
 
   const prompt =
-  PromptTemplate.fromTemplate(`Based on the provided SQL table schema below, write a SQL query that would answer the user's question.
+    PromptTemplate.fromTemplate(`Based on the provided SQL table schema below, write a SQL query that would answer the user's question.
     ------------
     SCHEMA: {schema}
     ------------
@@ -47,19 +44,17 @@ async function getResponse() {
     model.bind({ stop: ["\nSQLResult:"] }),
     new StringOutputParser(),
   ]);
-  
+
   const res = await sqlQueryChain.invoke({
     question: "What is the name of the second student in the students table?",
   });
-    console.log({ res });
-    console.log("complete");
-
+  console.log({ res });
+  console.log("complete");
 }
 
-AppDataSource.initialize().then(async () => {
-
-    console.log("init")
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("init");
     getResponse();
-
-}).catch(error => console.log(error))
-
+  })
+  .catch((error) => console.log(error));
